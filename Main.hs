b30e710ebf7e76ad00cb0b958f9ed92745a8f86f
Arne Link <mail@arne-link.de>
2013-12-13 22:18:48 +0100

    Weighted averages...

--- Main.hs
+++ Main.hs
@@ -16,72 +16,79 @@ main = do
     let rows    = C8.split '\n' bs
         llist   = map (C8.split ',') rows
         dMatrix = fromLists (map (map (read . C8.unpack)) llist :: [[Int]])
-        lt m    = los m (1, 1, 0) (400, 300, 0)
+        lt m    = los m (P 1 1 0) (P 400 300 0)
 
     putStrLn "looking for path..."
-    print $ map (\(x, y, _) -> [x, y]) $ runAStar dMatrix
+    print $ map (\(P x y _) -> [x, y]) $ runAStar dMatrix
     -- print $ lt dMatrix
     -- defaultMain [ bench "AStar" $ nf runAStar dMatrix
     --             , bench "los" $ nf lt dMatrix ]
     return ()
 
-heuristic :: DPoint -> Point -> Double
-heuristic (u, v, _) (x, y, _) = (sqrt((x' - u) ** 2 + (y' - v) ** 2)) * 115
-  where x'    = fromIntegral x
-        y'    = fromIntegral y
+-- heuristic :: DPoint -> Point -> Double
+-- heuristic (u, v, _) (x, y, _) = (sqrt((x' - u) ** 2 + (y' - v) ** 2)) * 90
+--   where x'    = fromIntegral x
+--         y'    = fromIntegral y
 
 los :: Map -> Point -> Point -> (Bool, Double)
-los m p@(a, _, _) q@(b, _, _) = (valid', fc)
-  where !((xa, ya, _), (xb, yb, _)) = if a < b then (p, q) else (q, p)
+los m p@(P a _ _) q@(P b _ _)
+    -- | D.trace ("coset: " ++ show cost ++ " points: " ++ show points ++ " l: " ++ show l) False = undefined
+    | otherwise = (valid', undefined)
+  where !((P xa ya _), (P xb yb _)) = if a < b then (p, q) else (q, p)
         !steep = abs(ya - yb) > abs(xa - xb)
         !(x0, x1) = if steep then (xb, xa) else (xa, xb)
         !(y0, y1) = if steep then (yb, ya) else (ya, yb)
-        !dx = x1 - x0
-        !dy = y1 - y0
-        !ddx = fromIntegral dx
-        !ddy = fromIntegral dy
-        !d0 = 2 * dy - dx
-        !fc = fromIntegral cost / fromIntegral points * l
-        !l = sqrt (ddx * ddx + ddy * ddy)
-        !c0 = m ! (x0, y0)
-        (valid', !cost, !points) = los' (x0 + 1) y0 d0 (True, c0, 1)
-        los' :: Int -> Int -> Int -> (Bool, Int, Int) -> (Bool, Int, Int)
-        los' !x !y !d (_, !c, !n)
-            | m ! (x, y) - 1 > c0 = (False, 0, n)
-            | x >= x1   = (True, nc, n+1)
-            | otherwise = los' nx ny nd (True, nc, n+1)
-                where !nx = x + 1
-                      !nd = (+) d (if d > 0 then 2*dy - 2*dx else 2*dy)
-                      !ny =        if d > 0 then y+1         else y
-                      !nc = c + fromIntegral (m ! (x, y))
+        !sx = x1 - x0
+        !sy = y1 - y0
+        !dx0 = abs $ 2 * (x0 - x1)
+        !dy0 = abs $ 2 * (y0 - y1)
+        !lowest = fromIntegral $ min (m ! (x0, y0)) (m ! (x1, y1))
+        (valid', !cost, !points) = los' (x0 + 1) y0 dx0 dy0 lowest (True, 0, 0)
+        los' :: Int -> Int -> Int -> Int -> Double -> Double -> (Bool, Int, Int) -> (Bool, Int, Int)
+        los' !x !y !dx !dy w low (clear, !c, !n)
+            -- | D.trace ("finished: " ++ show (x >= x1) ++ ", canceled: " ++ show (high - 1 > low)  ++ ", low: " ++ show low ++ ", current: " ++ show (m ! (x, y)) ) False = undefined
+            | x >= x1   = (clear, nc, n+1)
+            | high - 15 > low = recurse False
+            | otherwise = recurse clear
+                where !nd = (+) dx (if dx > 0 then 2*(dy-dx) else 2*dy)
+                      !ny =         if dx > 0 then y+1       else y
+                      !c' = m ! (x, y)
+                      !nc = c + c'
+                      !low' = min low $ fromIntegral c'
+                      !high = fromIntegral c'
+                      recurse cl = los' nx ny nx ndy low' (clear, nc, n+1)
 
 successor :: Map -> Point -> [Point]
 successor m = successor'
-  where inM (x, y, _) = y <= r && x <= c && y > 0 && x > 0 && (m ! (x, y) /= 255)
+  where inM (P x y _) = y <= r && x <= c && y > 0 && x > 0 && (m ! (x, y) /= 255)
         !r = nrows m
         !c = ncols m
-        successor' (x, y, _) = filter inM candidates
-          where candidates = [(x-1, y-1, 0), (x, y-1, 0), (x+1, y-1, 0)
-                             ,(x-1, y  , 0),              (x+1, y,   0)
-                             ,(x-1, y+1, 0), (x, y+1, 0), (x+1, y+1, 0)]
+        successor' (P x y _) = filter inM candidates
+          where candidates = [(P (x-1) (y-1) 0), (P x (y-1) 0), (P (x+1) (y-1) 0)
+                             ,(P (x-1) y   0),                  (P (x+1) y   0)
+                             ,(P (x-1) (y+1) 0), (P x (y+1) 0), (P (x+1) (y+1) 0)]
 
 cost :: Map -> Point -> Point -> Point -> [Point]
-cost m a@(_, _, ca) b@(x, y, cb) c@(u, v, _) = path
+cost m a@(P x0 y0 ca) b@(P x y cb) c@(P u v _)
+  | D.trace ("coord: " ++ show [a,b,c] ++ " smaller: " ++ show (ca > cb) ++ " d: " ++ show (round d') ++ " dir: " ++ show (round $ directCost + cb) ++ " los: " ++ show (round $ losD + ca)) False = path
+  | otherwise = path
   where (hasLos, losD) = los m a c
         directCost = rawCost * d
-        rawCost    = fromIntegral $ m ! (u, v)
+        rawCost    = abs $ fromIntegral $ m ! (u, v)
         path       = if hasLos then [c',a] else [c',b,a]
-        c' = (u, v, nc)
+        c' = (P u v nc)
         !nc = if hasLos then directCost + cb else losD + ca
-        d  = sqrt $ fromIntegral (dx + dy)
-        dx = abs $ x - u
-        dy = abs $ y - v
+        d  = abs $ sqrt $ (dx ** 2 + dy ** 2)
+        d' = abs $ sqrt $ (dx' ** 2 + dy' ** 2)
+        dx = fromIntegral $ x - u
+        dy = fromIntegral $ y - v
+        dx' = fromIntegral $ x0 - u
+        dy' = fromIntegral $ y0 - v
 
 runAStar :: Map -> [Point]
-runAStar m = astar start (isEnd end) succ (cost m) h m
- where start = (21, 495, 0)
-       end   = (344, 395, 0)
-       isEnd (x, y, _) (u, v, _) = x == u && y == v
-       dEnd  = (344, 395, 0)
+runAStar m = astar start (== end) succ (cost m) h m
+ where start = (P 21 495 (fromIntegral $ m ! (21, 495)))
+       end   = (P 344 395 0)
+       dEnd  = (DP 344 395 0)
        succ  = successor m
-       h     = heuristic dEnd
+       h     = (* 0.8) . snd . los m end
--- src/AStar.hs
+++ src/AStar.hs
@@ -12,32 +12,49 @@ import qualified Data.PQueue.Prio.Min    as Q
 import           Debug.Trace             as D
 -- import           Prelude
 
-type Point   = (Int, Int, Double)
-type DPoint  = (Double, Double, Double)
+data Point = P
+             {-# UNPACK #-} !Int
+             {-# UNPACK #-} !Int
+             {-# UNPACK #-} !Double
+             deriving (Ord)
+instance Eq Point where
+  (P a1 b1 _) == (P a2 b2 _) = a1 == a2 && b1 == b2
+instance Show Point where
+  show (P x y c) = "(" ++ show x ++ "," ++ show y++"):" ++ show (round c :: Int)
+
+data DPoint  = DP
+               {-# UNPACK #-} !Int
+               {-# UNPACK #-} !Int
+               {-# UNPACK #-} !Double
+               deriving (Show, Ord)
+instance Eq DPoint where
+  (DP a1 b1 _) == (DP a2 b2 _) = a1 == a2 && b1 == b2
+
+
 
 type Map = Matrix Int
 
-trd (_, _, t) = t
+getCost (P _ _ t) = t
 
 astar :: Point               -- ^ start point
       -> (Point -> Bool)     -- ^ Point is End?
       -> (Point -> [Point])  -- ^ succ function
       -> (Point -> Point -> Point -> [Point])
-      -> (Point -> Double)
-      -> Matrix m
+      -> (Point -> Double)   -- ^ heuristic function
+      -> Matrix m            -- ^ the playground
       -> [Point]
 astar start end succ cost heur m
     = astar' (S.singleton $ toInt start) (Q.singleton (heur start) [start,start])
   where r = nrows m
-        toInt (x, y, _) = x * r + y
+        toInt (P x y _) = x * r + y
         astar' !seen !q
-            | D.trace ((show $ end l) ++ " - " ++ (show $ Q.findMin q) ++ "\n") False = undefined
+            -- | D.trace (show (end l) ++ " - " ++ (show $ Q.findMin q) ++ "\n") False = undefined
             | Q.null q  = error "No Solution."
             | end l     = path
             | otherwise = astar' seen' q'
           where ((c, path@(l:pre:rest)), dq) = Q.deleteFindMin q
                 !succs = filter ((`S.notMember` seen) . toInt) $ succ l
                 !costs = map (cost pre l ) succs
-                pq     = map (\np@(n:_) -> (c - (heur l) + (heur n), np ++ rest)) costs
+                pq     = map (\np@(n:_) -> ((getCost n) + (heur n), np ++ rest)) costs
                 !q'    = dq `Q.union` Q.fromList pq
                 !seen' = seen `S.union` S.fromList (map toInt succs)
