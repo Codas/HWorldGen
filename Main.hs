af80c65a2cbb4b8d0b813e6a24e2ee590c74f4d4
Arne Link <mail@arne-link.de>
2013-12-18 18:20:05 +0100

    start drawing paths

--- Main.hs
+++ Main.hs
@@ -1,94 +1,41 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns              #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 module Main where
 
 import           AStar
-import           Control.DeepSeq
-import qualified Data.ByteString       as BS
-import qualified Data.ByteString.Char8 as C8
-import           Data.Matrix           as M
+import qualified Data.ByteString              as BS
+import qualified Data.ByteString.Char8        as C8
+import           Data.Matrix                  as M
 
+import           Criterion.Config
 import           Criterion.Main
-import           Debug.Trace           as D
+import           Diagrams.Backend.SVG         (renderSVG)
+import           Diagrams.Backend.SVG.CmdLine
+import           Diagrams.Prelude
+-- import           Debug.Trace           as D
 
 main :: IO ()
 main = do
-    bs <- BS.readFile "terrain.txt"
-    let rows    = C8.split '\n' bs
-        llist   = map (C8.split ',') rows
-        dMatrix = fromLists (map (map (read . C8.unpack)) llist :: [[Int]])
-        lt m    = los m (P 1 1 0) (P 400 300 0)
+    m <- readWorld
+    let start = P 21 495 0
+        !end  = P 344 395 0
+        path  = findPath m start end
+        str   = map (\(P x y _) -> [x, y]) $ path
+        size  = Absolute
+        pathDiag = fromVertices (pathToPoints path) # strokeLine
+                   # lc red # lw 2
 
     putStrLn "looking for path..."
-    print $ map (\(P x y _) -> [x, y]) $ runAStar dMatrix
-    -- print $ lt dMatrix
-    -- defaultMain [ bench "AStar" $ nf runAStar dMatrix
-    --             , bench "los" $ nf lt dMatrix ]
+    print path
+    renderSVG "path.svg" size $ pathDiag `atop` square 804
     return ()
 
--- heuristic :: DPoint -> Point -> Double
--- heuristic (u, v, _) (x, y, _) = (sqrt((x' - u) ** 2 + (y' - v) ** 2)) * 90
---   where x'    = fromIntegral x
---         y'    = fromIntegral y
+pathToPoints path = reverse (map (\(P x y _) -> (fromIntegral x) ^& (804 - fromIntegral y)) path)
 
-los :: Map -> Point -> Point -> (Bool, Double)
-los m p@(P a _ _) q@(P b _ _)
-    -- | D.trace ("coset: " ++ show cost ++ " points: " ++ show points ++ " l: " ++ show l) False = undefined
-    | otherwise = (valid', undefined)
-  where !((P xa ya _), (P xb yb _)) = if a < b then (p, q) else (q, p)
-        !steep = abs(ya - yb) > abs(xa - xb)
-        !(x0, x1) = if steep then (xb, xa) else (xa, xb)
-        !(y0, y1) = if steep then (yb, ya) else (ya, yb)
-        !sx = x1 - x0
-        !sy = y1 - y0
-        !dx0 = abs $ 2 * (x0 - x1)
-        !dy0 = abs $ 2 * (y0 - y1)
-        !lowest = fromIntegral $ min (m ! (x0, y0)) (m ! (x1, y1))
-        (valid', !cost, !points) = los' (x0 + 1) y0 dx0 dy0 lowest (True, 0, 0)
-        los' :: Int -> Int -> Int -> Int -> Double -> Double -> (Bool, Int, Int) -> (Bool, Int, Int)
-        los' !x !y !dx !dy w low (clear, !c, !n)
-            -- | D.trace ("finished: " ++ show (x >= x1) ++ ", canceled: " ++ show (high - 1 > low)  ++ ", low: " ++ show low ++ ", current: " ++ show (m ! (x, y)) ) False = undefined
-            | x >= x1   = (clear, nc, n+1)
-            | high - 15 > low = recurse False
-            | otherwise = recurse clear
-                where !nd = (+) dx (if dx > 0 then 2*(dy-dx) else 2*dy)
-                      !ny =         if dx > 0 then y+1       else y
-                      !c' = m ! (x, y)
-                      !nc = c + c'
-                      !low' = min low $ fromIntegral c'
-                      !high = fromIntegral c'
-                      recurse cl = los' nx ny nx ndy low' (clear, nc, n+1)
-
-successor :: Map -> Point -> [Point]
-successor m = successor'
-  where inM (P x y _) = y <= r && x <= c && y > 0 && x > 0 && (m ! (x, y) /= 255)
-        !r = nrows m
-        !c = ncols m
-        successor' (P x y _) = filter inM candidates
-          where candidates = [(P (x-1) (y-1) 0), (P x (y-1) 0), (P (x+1) (y-1) 0)
-                             ,(P (x-1) y   0),                  (P (x+1) y   0)
-                             ,(P (x-1) (y+1) 0), (P x (y+1) 0), (P (x+1) (y+1) 0)]
-
-cost :: Map -> Point -> Point -> Point -> [Point]
-cost m a@(P x0 y0 ca) b@(P x y cb) c@(P u v _)
-  | D.trace ("coord: " ++ show [a,b,c] ++ " smaller: " ++ show (ca > cb) ++ " d: " ++ show (round d') ++ " dir: " ++ show (round $ directCost + cb) ++ " los: " ++ show (round $ losD + ca)) False = path
-  | otherwise = path
-  where (hasLos, losD) = los m a c
-        directCost = rawCost * d
-        rawCost    = abs $ fromIntegral $ m ! (u, v)
-        path       = if hasLos then [c',a] else [c',b,a]
-        c' = (P u v nc)
-        !nc = if hasLos then directCost + cb else losD + ca
-        d  = abs $ sqrt $ (dx ** 2 + dy ** 2)
-        d' = abs $ sqrt $ (dx' ** 2 + dy' ** 2)
-        dx = fromIntegral $ x - u
-        dy = fromIntegral $ y - v
-        dx' = fromIntegral $ x0 - u
-        dy' = fromIntegral $ y0 - v
-
-runAStar :: Map -> [Point]
-runAStar m = astar start (== end) succ (cost m) h m
- where start = (P 21 495 (fromIntegral $ m ! (21, 495)))
-       end   = (P 344 395 0)
-       dEnd  = (DP 344 395 0)
-       succ  = successor m
-       h     = (* 0.8) . snd . los m end
+readWorld :: IO (Matrix Int)
+readWorld = do
+    bs <- BS.readFile "terrain.txt"
+    let rows  = C8.split '\n' bs
+        llist = map (C8.split ',') rows
+        m     = fromLists (map (map (read . C8.unpack)) llist :: [[Int]])
+    return m
--- src/AStar.hs
+++ src/AStar.hs
@@ -1,15 +1,11 @@
 {-# LANGUAGE BangPatterns #-}
 module AStar where
 
-import           Control.DeepSeq
-import           Control.Monad           (liftM2)
-import           Control.Monad.Instances
-import           Data.IntSet             (IntSet)
-import qualified Data.IntSet             as S
-import           Data.List               (foldl')
-import           Data.Matrix             (Matrix, nrows)
-import qualified Data.PQueue.Prio.Min    as Q
-import           Debug.Trace             as D
+import           Data.IntSet          (IntSet)
+import qualified Data.IntSet          as S
+import           Data.Matrix          (Matrix, ncols, nrows, (!))
+import qualified Data.PQueue.Prio.Min as Q
+-- import           Debug.Trace          as D
 -- import           Prelude
 
 data Point = P
@@ -22,18 +18,18 @@ instance Eq Point where
 instance Show Point where
   show (P x y c) = "(" ++ show x ++ "," ++ show y++"):" ++ show (round c :: Int)
 
+
 data DPoint  = DP
-               {-# UNPACK #-} !Int
-               {-# UNPACK #-} !Int
+               {-# UNPACK #-} !Double
+               {-# UNPACK #-} !Double
                {-# UNPACK #-} !Double
                deriving (Show, Ord)
 instance Eq DPoint where
   (DP a1 b1 _) == (DP a2 b2 _) = a1 == a2 && b1 == b2
 
-
-
 type Map = Matrix Int
 
+getCost :: Point -> Double
 getCost (P _ _ t) = t
 
 astar :: Point               -- ^ start point
@@ -58,3 +54,84 @@ astar start end succ cost heur m
                 pq     = map (\np@(n:_) -> ((getCost n) + (heur n), np ++ rest)) costs
                 !q'    = dq `Q.union` Q.fromList pq
                 !seen' = seen `S.union` S.fromList (map toInt succs)
+
+
+heuristic :: Point -> Point -> Double
+heuristic (P u v _) (P x y _) = (sqrt((x' - u') ** 2 + (y' - v') ** 2)) * 90
+  where x'    = fromIntegral x
+        y'    = fromIntegral y
+        u'    = fromIntegral u
+        v'    = fromIntegral v
+
+los :: Map -> Point -> Point -> (Bool, Double)
+los m (P startX startY _) (P endX endY _) = (valid', fc)
+  where (vxi, vyi) = (endX - startX, endY - startY)
+        !switch = abs (endX - startX) < abs (endY - startY)
+        tileCost (tx, ty) = m ! if switch then (ty, tx) else (tx, ty)
+        !(vx, vy) = if switch then (vyi, vxi) else (vxi, vyi)
+        !(x0, y0) = if switch then (startY, startX) else (startX, startY)
+        !(x1, y1) = if switch then (endY, endX) else (endX, endY)
+        !sx  = signum vx   -- signum funktion, negativ: -1, 0: 0, positiv: +1
+        !sy  = signum vy   -- Schritte in x, y richtung
+        !dx = abs $ 2 * vx -- absolutes delta, wieso *2?
+        !dy = abs $ 2 * vy
+        !c0  = - tileCost (x0, y0)  -- Initial cost
+        !e0  = 0  -- initial error
+        !w0  = 0
+        !p   = (dy + dx) `div` 2
+        !t   = (dy - dx) `div` 2
+        fc  = cost * ca
+        ca = if dx /= 0 then sqrt (fromIntegral dx ** 2 + fromIntegral dy ** 2) / fromIntegral dx else cost
+        !lowest = fromIntegral $ min (m ! (x0, y0)) (m ! (x1, y1)) -- lowest current cost.
+        (valid', !cost) = los' x0 y0 c0 e0 w0 lowest True  -- begin iteration
+        los' :: Int -> Int -> Int -> Int -> Int -> Int -> Bool -> (Bool, Double)
+        los' !x !y !c !e !w !low clear
+            | x == x1   = (clear, fromIntegral nc)            -- When x == x1, return cleat status and costs
+            | high - 15 > low = recurse False    -- when worse terrain encountered, set LOS to false, but continue
+            | otherwise = recurse clear          -- we're not done yet, continue
+                where !ne | e > t = e - dy
+                          | e < t = e + dx
+                          | otherwise = e + dx - dy
+                      !nc | e > t && p + e < dy = c + w * c'
+                          | e < t = c + (1-w) * c'
+                          | otherwise = c + c'
+                      !c' = tileCost (x, y) -- current field costs
+                      !nw = if (e > t) && (p + e < dy) then (p + e) `div` dy else w
+                      !nx = if e < t then x else x + sx
+                      !ny = if e > t then y else y + sy
+                      !low' = min low c'                  -- new cost minimum
+                      !high = c'  -- naming alias to make things more explicit
+                      recurse = los' nx ny nc ne nw low'  -- continue with LOS status
+
+successor :: Map -> Point -> [Point]
+successor m = successor'
+  where inM (P x y _) = y <= r && x <= c && y > 0 && x > 0 && (m ! (x, y) /= 255)
+        !r = nrows m
+        !c = ncols m
+        successor' (P x y _) = filter inM candidates
+          where candidates = [(P (x-1) (y-1) 0), (P x (y-1) 0), (P (x+1) (y-1) 0)
+                             ,(P (x-1) y   0),                  (P (x+1) y   0)
+                             ,(P (x-1) (y+1) 0), (P x (y+1) 0), (P (x+1) (y+1) 0)]
+
+cost :: Map -> Point -> Point -> Point -> [Point]
+cost m a@(P x0 y0 ca) b@(P x y cb) c@(P u v _)
+  -- | D.trace ("coord: " ++ show [a,b,c] ++ " smaller: " ++ show (ca > cb) ++ " dir: " ++ show (round $ directCost + cb) ++ " los: " ++ show (round $ losD + ca)) False = path
+  | otherwise = path
+  where (hasLos, losD) = los m a c
+        directCost = rawCost * d + swCost
+        rawCost    = abs $ fromIntegral $ m ! (u, v)
+        path       = if hasLos then [c',a] else [c',b,a]
+        swCost     = fromIntegral $ (abs $ (m ! (u, v)) - (m ! (x, y))) * 4
+        c' = (P u v nc)
+        !nc = if hasLos && losD + ca < directCost + cb then losD + ca else directCost + cb
+        d  = abs $ sqrt $ (dx ** 2 + dy ** 2)
+        d' = abs $ sqrt $ (dx' ** 2 + dy' ** 2)
+        dx = fromIntegral $ x - u
+        dy = fromIntegral $ y - v
+        dx' = fromIntegral $ x0 - u
+        dy' = fromIntegral $ y0 - v
+
+findPath :: Map -> Point -> Point -> [Point]
+findPath m start end = astar start (== end) succ (cost m) h m
+ where succ  = successor m
+       h     = heuristic end
